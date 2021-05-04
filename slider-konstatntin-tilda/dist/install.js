36a92a7f9b2b34122f7d9ec166c9a92b031cda72
Den <danil.savchenko.dev@gmail.com>
2021-05-04 19:14:14 +0600

    Update

--- slider-konstatntin-tilda/dist/install.js
+++ slider-konstatntin-tilda/dist/install.js
@@ -1,5 +1,5 @@
 $(function() {
-  const hash = '01827cb826958c595ef1'
+  const hash = '652f0e72ef390f30298d'
   const devMode = window.location.hostname.split('.').reverse()[0] === 'ws'
 
   const script = document.createElement('script')
