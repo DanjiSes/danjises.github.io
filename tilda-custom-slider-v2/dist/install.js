b37230d98a54741359b3fa18977e22782468c742
Savchenko <danil.savchenko.dev@gmail.com>
2022-07-08 23:50:11 +0600

    Add new slider

---
+++ tilda-custom-slider-v2/dist/install.js
@@ -0,0 +1,22 @@
+$(function () {
+  const hash = "8aab32e24c91605a3669";
+  const devMode = window.__shDevMode__ || false;
+
+  const script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = devMode
+    ? "http://savchenko.sl:3000/bundle.js"
+    : "https://danjises.github.io/slider-konstatntin-tilda/dist/bundle." +
+      hash +
+      ".js";
+
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = devMode
+    ? "http://savchenko.sl:3000/bundle.css"
+    : "https://danjises.github.io/slider-konstatntin-tilda/dist/bundle." +
+      hash +
+      ".css";
+
+  $("body").append([script, link]);
+});
