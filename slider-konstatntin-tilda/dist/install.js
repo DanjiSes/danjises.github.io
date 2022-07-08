cd6daf10366fd2099c59556d7ef721c99e8099b8
Savchenko <danil.savchenko.dev@gmail.com>
2022-07-08 11:49:24 +0600

    .

--- slider-konstatntin-tilda/dist/install.js
+++ slider-konstatntin-tilda/dist/install.js
@@ -1,14 +1,22 @@
-$(function() {
-  const hash = 'be0cb43820716a125e12'
-  const devMode = window.location.hostname.split('.').reverse()[0] === 'ws'
+$(function () {
+  const hash = "be0cb43820716a125e12";
+  const devMode = false;
 
-  const script = document.createElement('script')
-  script.type = 'text/javascript'
-  script.src = devMode ? 'http://savchenko.sl:3000/bundle.js' : 'https://danjises.github.io/slider-konstatntin-tilda/dist/bundle.' + hash + '.js'
+  const script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = devMode
+    ? "http://savchenko.sl:3000/bundle.js"
+    : "https://danjises.github.io/slider-konstatntin-tilda/dist/bundle." +
+      hash +
+      ".js";
 
-  const link = document.createElement('link')
-  link.rel = 'stylesheet'
-  link.href = devMode ? 'http://savchenko.sl:3000/bundle.css' : 'https://danjises.github.io/slider-konstatntin-tilda/dist/bundle.' + hash + '.css'
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = devMode
+    ? "http://savchenko.sl:3000/bundle.css"
+    : "https://danjises.github.io/slider-konstatntin-tilda/dist/bundle." +
+      hash +
+      ".css";
 
-  $('body').append([script, link])
-})
+  $("body").append([script, link]);
+});
